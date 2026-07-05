498ca3430cc06ad6ec90e29f983269474020588e
Nobel Alemayehu <nobbbiealex@gmail.com>
2026-07-05 18:32:48 +0300

    Enhance connection routes with additional endpoints
    Added routes for getting pending requests and accepting/declining connections.

--- routes/connectionRoutes.js
+++ routes/connectionRoutes.js
@@ -3,5 +3,8 @@ const connectionController = require('../controller/connectionController');
 const authMiddleware = require('../middleware/authMiddleware');
 
 router.post('/request', authMiddleware, connectionController.sendConnectionRequest);
+router.get('/pending', authMiddleware, connectionController.getPendingRequests);
+router.post('/accept/:id', authMiddleware, connectionController.acceptConnection);
+router.post('/decline/:id', authMiddleware, connectionController.declineConnection);
 
 module.exports = router;
