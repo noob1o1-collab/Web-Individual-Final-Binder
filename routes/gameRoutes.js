ca710cba4d687c2759b7778feee301080b722f30
Nobel Alemayehu <nobbbiealex@gmail.com>
2026-07-09 08:44:59 +0300

    Delete routes/gameRoutes.js

--- routes/gameRoutes.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const gameController = require('../controller/gameController');
-const authMiddleware = require('../middleware/authMiddleware');
-
-router.get('/state', authMiddleware, gameController.getGameState);
-router.post('/move', authMiddleware, gameController.makeMove);
-router.post('/reset', authMiddleware, gameController.resetGame);
-
-module.exports = router;
