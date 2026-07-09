c1588b3935391a1cf3c14c5f1a38e9ab19d0ffa7
Nobel Alemayehu <nobbbiealex@gmail.com>
2026-07-09 12:54:17 +0300

    Update connectionRoutes.js

--- routes/connectionRoutes.js
+++ routes/connectionRoutes.js
@@ -7,6 +7,9 @@ router.get('/pending', authMiddleware, connectionController.getPendingRequests);
 router.post('/accept/:id', authMiddleware, connectionController.acceptConnection);
 router.post('/decline/:id', authMiddleware, connectionController.declineConnection);
 
+router.get('/pending', authMiddleware, connectionController.getPendingRequests);
 router.get('/status', authMiddleware, connectionController.getConnectionStatus);
+router.get('/accepted', authMiddleware, connectionController.getAcceptedConnections);
+router.get('/detail', authMiddleware, connectionController.getConnectionDetail);
 
 module.exports = router;
